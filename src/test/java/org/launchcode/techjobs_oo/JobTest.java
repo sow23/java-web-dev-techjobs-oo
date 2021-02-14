7b7b92c4ec5eda0c1bf5a2bd4806faa1a4bd5dfa
sow23 <sow23@gmail.com>
2021-02-14 13:18:17 -0600

    added maven repo for junit 5 dependencies

---
+++ src/test/java/org/launchcode/techjobs_oo/JobTest.java
@@ -0,0 +1,17 @@
+package org.launchcode.techjobs_oo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JobTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
